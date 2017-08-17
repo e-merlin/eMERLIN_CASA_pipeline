70c1656cd1335509e8edde2de609e9cf25e70493
Javier Moldon <javier.moldon@manchester.ac.uk>
2017-08-17 11:38:44 +0100

    Changed 'vis' to 'msfile' to avoid any conflict with names.

--- eMERLIN_CASA_pipeline.py
+++ eMERLIN_CASA_pipeline.py
@@ -50,7 +50,7 @@ if inputs['quit'] == 1: #Check from GUI if quit is needed
     sys.exit()
 
 fitsfile = inputs['inbase']+'.fits'
-vis = inputs['inbase']+'.ms'
+msfile = inputs['inbase']+'.ms'
 
 
 ## Check for measurement sets in current directory otherwise drag from defined data directory
@@ -62,36 +62,36 @@ if os.path.isdir(inputs['inbase']+'.ms') == False and os.path.isdir(inputs['inba
 	else:
 		logger.info('No measurement set found, assuming you need to importfits or change data dir')
 else:
-	logger.info('Measurement set found: {}. Continuing with your inputs'.format(vis))
+	logger.info('Measurement set found: {}. Continuing with your inputs'.format(msfile))
 
 ## Pipeline processes, inputs are read from the inputs dictionary
 if inputs['run_importfits'] == 1:
-    em.run_importfitsIDI(data_dir,vis)
-    em.check_mixed_mode(vis,mode='split')
+    em.run_importfitsIDI(data_dir,msfile)
+    em.check_mixed_mode(msfile,mode='split')
 
 if inputs['hanning'] == 1:
-	em.hanning(inputvis=vis,deloriginal=True)
+	em.hanning(inputvis=msfile,deloriginal=True)
 
 if inputs['rfigui'] == 1:
-    em.run_rfigui(vis)
+    em.run_rfigui(msfile)
 
 ### Convert MS to MMS ###
 if inputs['ms2mms'] == 1:
     if em.check_aoflagger_version():
-        em.ms2mms_fields(msfile=vis)
+        em.ms2mms_fields(msfile=msfile)
     else:
-        em.ms2mms(vis=vis,mode='parallel')
+        em.ms2mms(vis=msfile,mode='parallel')
 
 ## check for parallelisation
 if os.path.isdir('./'+inputs['inbase']+'.mms') == True:
-    vis = inputs['inbase']+'.mms'
+    msfile = inputs['inbase']+'.mms'
 
 if inputs['autoflag'] == 1:
-    em.run_aoflagger_fields(vis=vis,fields='all', pipeline_path = pipeline_path)
+    em.run_aoflagger_fields(vis=msfile,fields='all', pipeline_path = pipeline_path)
 
 ### Produce some initial plots ###
 if inputs['do_prediag'] == 1:
-	em.do_prediagnostics(vis,plots_dir)
+	em.do_prediagnostics(msfile,plots_dir)
 
 previous_cal = []
 previous_spwmap = []
@@ -99,16 +99,16 @@ previous_spwmap = []
 ### Delay calibration ###
 if inputs['do_delay'] == 1:
     delay_caltable = inputs['inbase']+'_delay.K'
-    caltable_delay, spwmap_out_delay = em.solve_delays(vis,caltable_name=delay_caltable,calsources='',solint='600s',refant=refant,combine='spw',spw='',caldir=calib_dir,plotdir=plots_dir)
+    caltable_delay, spwmap_out_delay = em.solve_delays(msfile,caltable_name=delay_caltable,calsources='',solint='600s',refant=refant,combine='spw',spw='',caldir=calib_dir,plotdir=plots_dir)
 
 if os.path.isdir(calib_dir+inputs['inbase']+'_delay.K'):
-    num_spw = len(vishead(vis, mode = 'list', listitems = ['spw_name'])['spw_name'][0])
+    num_spw = len(vishead(msfile, mode = 'list', listitems = ['spw_name'])['spw_name'][0])
     previous_cal.append(calib_dir+inputs['inbase']+'_delay.K')
     previous_spwmap.append([0]*num_spw)
 
 ### Initial BandPass calibration ###
 if inputs['do_initial_bandpass'] == 1:
-    em.initial_bp_cal(msfile=vis, bpcal=bpcal, refant=refant, caldir=calib_dir, plotdir=plots_dir, previous_cal=previous_cal,  previous_spwmap=previous_spwmap)
+    em.initial_bp_cal(msfile=msfile, bpcal=bpcal, refant=refant, caldir=calib_dir, plotdir=plots_dir, previous_cal=previous_cal,  previous_spwmap=previous_spwmap)
 
 
 logger.info('Pipeline finished')
