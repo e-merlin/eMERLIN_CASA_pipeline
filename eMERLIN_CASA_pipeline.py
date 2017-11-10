8ead503523eb0e8ae4e21add0d75b408f5ece086
Javier Moldon <jmoldon@users.noreply.github.com>
2017-11-10 15:11:36 +0000

    Update version to v0.6.6

--- eMERLIN_CASA_pipeline.py
+++ eMERLIN_CASA_pipeline.py
@@ -10,7 +10,7 @@ import logging
 from taskinit import *
 from tasks import *
 
-pipeline_version = 'v0.6.5'
+pipeline_version = 'v0.6.6'
 
 # Find path of pipeline to find external files (like aoflagger strategies or emerlin-2.gif)
 try:
