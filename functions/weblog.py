d6e0733f223cdf893b6c928314a721c0a9a3d0be
Javier Moldon <javier.moldon@manchester.ac.uk>
2017-12-07 14:45:52 +0000

    Fixed bug on weblog when showing the separation between some target-phasecal pairs due to the order

--- functions/weblog.py
+++ functions/weblog.py
@@ -106,7 +106,10 @@ def weblog_obssum(msinfo):
                 logger.warning('{} not in MS'.format(s2))
                 separation = 'Not in MS'
             else:
-                separation = '{0:5.2f}'.format(separations[s1+'-'+s2])
+                try:
+                    separation = '{0:5.2f}'.format(separations[s1+'-'+s2])
+                except:
+                    separation = '{0:5.2f}'.format(separations[s2+'-'+s1])
             wlog.write('<tr><td>{0} </td><td> {1}</td><td>{2}</td>\n'.format(s1, s2, separation))
         wlog.write('</table><br>\n')
     wlog.write('<table bgcolor="#eeeeee" border="3px" cellspacing = "0" cellpadding = "4px" style="width:30%">\n')
