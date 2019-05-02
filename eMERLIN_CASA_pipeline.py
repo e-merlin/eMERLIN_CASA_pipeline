a654264d29e9dae6cc870da71927a4a9c48e8a1f
Javier Moldon <javier.moldon@manchester.ac.uk>
2019-05-02 13:11:59 +0100

    v1.0.0

--- eMERLIN_CASA_pipeline.py
+++ eMERLIN_CASA_pipeline.py
@@ -13,7 +13,7 @@ from tasks import *
 import casadef
 
 
-current_version = 'v0.10.25'
+current_version = 'v1.0.0'
 
 # Find path of pipeline to find external files (like aoflagger strategies or emerlin-2.gif)
 try:
